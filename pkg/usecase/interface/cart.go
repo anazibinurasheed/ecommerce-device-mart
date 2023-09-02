49925e132c02a30145adb6fec3015f982570d2fe
anazibinurasheed <anazibinurasheed@gmail.com>
2023-09-02 15:54:37 +0530

    Started adding godoc comments.

--- pkg/usecase/interface/cart.go
+++ pkg/usecase/interface/cart.go
@@ -3,10 +3,21 @@ package interfaces
 import "github.com/anazibinurasheed/project-device-mart/pkg/util/response"
 
 type CartUseCase interface {
+	// AddToCart adds a product to the user's shopping cart.
 	AddToCart(userID, ProductID int) error
+
+	// ViewCart retrieves the items in the user's shopping cart.
 	ViewCart(userID int) (response.CartItems, error)
+
+	// RemoveFromCart removes a product from the user's shopping cart.
 	RemoveFromCart(userID, productID int) error
+
+	// IncrementQuantity increases the quantity of a product in the user's cart.
 	IncrementQuantity(userID, productID int) error
+
+	// DecrementQuantity decreases the quantity of a product in th user's cart.
 	DecrementQuantity(userID, productID int) error
+
+	// DeleteUserCart deletes the entire shopping cart of a user.
 	DeleteUserCart(userID int) error
 }
